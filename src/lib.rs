d9842c71f0716deb6fbf63eb2407a30557b7a0d2
Jakub Hess <jakub.hess@livereader.com>
2025-10-31 15:00:27 +0100

    fix return correct value from crate_inference_session

--- src/lib.rs
+++ src/lib.rs
@@ -66,13 +66,13 @@ fn load_model_config(config_path: &Path) -> PiperResult<(ModelConfig, PiperSynth
 
 fn create_inference_session(model_path: &Path) -> Result<Session, ort::Error> {
     let exec_provider = execution_providers::CPUExecutionProvider::default().build();
-    let session = Session::builder()?
+    Session::builder()?
         // .with_parallel_execution(true)?
         // .with_inter_threads(16)?
         // .with_optimization_level(ort::GraphOptimizationLevel::Level3)?
         .with_memory_pattern(false)?
         .with_execution_providers([exec_provider])?
-        .commit_from_file(model_path);
+        .commit_from_file(model_path)
 }
 
 pub fn from_config_path(config_path: &Path) -> PiperResult<Arc<dyn PiperModel + Send + Sync>> {
